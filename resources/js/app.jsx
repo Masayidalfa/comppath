1b1579451933b6445e7669f2cf41b6a545f8eb33
ihwanulfikriramadhan7 <ihwanulfikriramadhan7@gmail.com>
2024-12-15 05:44:11 +0700

    React js terpasang

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1>Hello, udah Move gk lu</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
