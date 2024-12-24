6e5c252c40296df33233f456925edc1e80b69295
Anisafatonati <114457553+Anisafatonati@users.noreply.github.com>
2024-12-24 21:23:14 +0700

    Menambahkan perubahan di App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 // import './App.css'
-// import Layout from './layout/admin/index'
+import Layout from './layout/admin/index'
 import { Routes, Route } from 'react-router-dom'
-import LayoutUser from './layout/user/IndexUser';
-import Registration from './pages/user/FormPendaftaran';
-// import Dashboard from './pages/admin/Dashboard'
-// import Kategori from './pages/admin/kategori/Kategori'
-// import User from './pages/admin/user/User'
-// import DetailUser from './pages/admin/detail_user/DetailUser'
-// import Lomba from './pages/admin/lomba/Lomba'
-// import Pendaftaran from './pages/admin/pendaftaran/Pendaftaran'
-// import KelolaLomba from './pages/admin/kelola_lomba/KelolaLomba'
-// import AddKategori from './pages/admin/kategori/addKategori'
+// import LayoutUser from './layout/user/IndexUser';
+// import Registration from './pages/user/FormPendaftaran';
+import Dashboard from './pages/admin/Dashboard'
+import Kategori from './pages/admin/kategori/Kategori'
+import User from './pages/admin/user/User'
+import DetailUser from './pages/admin/detail_user/DetailUser'
+import Lomba from './pages/admin/lomba/Lomba'
+import Pendaftaran from './pages/admin/pendaftaran/Pendaftaran'
+import KelolaLomba from './pages/admin/kelola_lomba/KelolaLomba'
+import AddKategori from './pages/admin/kategori/addKategori'
 function App() {
   return (
-    <LayoutUser>
+    // <LayoutUser>
+    <Layout>
       <Routes>
-        <Route path='/' element={<Registration/>}></Route>
-        {/* <Route path="/" element={<Dashboard/>}></Route>
+        {/* <Route path='/' element={<Registration/>}></Route> */}
+        <Route path="/" element={<Dashboard/>}></Route>
         <Route path="/kategori" element={<Kategori/>}></Route>
         <Route path="/user" element={<User/>}></Route>
         <Route path="/detail-user" element={<DetailUser/>}></Route>
@@ -24,9 +25,10 @@ function App() {
         <Route path="/pendaftaran" element={<Pendaftaran/>}></Route>
         <Route path="/kelola-lomba" element={<KelolaLomba/>}></Route>
         <Route path="/addKategori" element={<AddKategori/>}></Route>
-        <Route path="*" element={<h1>404</h1>}></Route> */}
+        <Route path="*" element={<h1>404</h1>}></Route>
       </Routes>
-      </LayoutUser>
+      {/* </LayoutUser> */}
+      </Layout>
   )
 }
 
