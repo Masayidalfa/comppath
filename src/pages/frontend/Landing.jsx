420cea41fc0f3bb6b8c01998a14f6e76cb518c99
Anisafatonati <114457553+Anisafatonati@users.noreply.github.com>
2024-12-30 21:54:10 +0700

    Mengambil about di landing

--- src/pages/frontend/Landing.jsx
+++ src/pages/frontend/Landing.jsx
@@ -1,38 +1,10 @@
+import About from "./About";
+
 function Landing(){
     return(
         <div>
             {/* content 1 */}
-            <div className="flex flex-row m-10 border-b border-gray-500">
-                <div className=" ">
-                    <h2 className="text-blue-600 mb-4 text-4xl">Temukan Lomba Impianmu dengan Satu Klik</h2>
-                        <p>“Lomba yang Pas, Untuk Kamu yang Unik. Jelajahi Ribuan Lomba di Comppath.”</p>
-                        <input type="text" className="border border-blue-300 px-20 py-1" placeholder="Cari Lomba"/>
-                        <button className="bg-blue-600 text-white px-10 py-1 " >Cari</button>
-                 </div>
-                 <div className="flex justify-end">
-                        <img 
-                        src="/image/photo2.png" 
-                        alt="" 
-                        className="object-cover sm:w-1/2"
-                        />
-                </div>
-                </div>
-            {/* content 2 */}
-            <div className="flex flex-row gap-x-4 m-10 bg-blue-100">
-                <div className="m-4">
-                    <img 
-                    src="./image/photo3.png"
-                    alt=""
-                    className="object-cover sm:max-w-96" 
-                    />
-                </div>
-            <div className="bg-blue-200 text-gray-800 p-4">
-                <h1 className="text-3xl font-bold text-center">Tentang CompPath!!!</h1>
-                <p className="text-lg mt-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
-        </div>
+            <About />
 
             {/* contetn 3 */}
             <div className="flex flex-col items-center m-10">
